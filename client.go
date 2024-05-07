a7906fde3005d8b7dacba170eb4735011c7335e7
shiguangpeng <742508176@qq.com>
2024-05-07 23:13:40 +0800

    :sparkles: v0.9客户端命令行输入参数并解析

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -29,10 +30,19 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
-// 连接到server
+var serverIP string
+var serverPort int
 
+// 命令行解析
+func init() {
+	flag.StringVar(&serverIP, "ip", "127.0.0.1", "设置服务器IP地址，默认是127.0.0.1")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口号地址，默认是8888")
+}
 func main() {
-	var client *Client = NewClient("127.0.0.1", 8888)
+	// 解析命令行
+	flag.Parse()
+
+	var client *Client = NewClient(serverIP, serverPort)
 	if client == nil {
 		fmt.Println(">>>>>> 连接服务器失败 <<<<<<<<<<")
 		return
