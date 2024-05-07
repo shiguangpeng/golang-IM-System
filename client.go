0f66fb0a7c680b297e60a2917d2290ae52dfd8ed
shiguangpeng <742508176@qq.com>
2024-05-08 06:40:48 +0800

    :sparkles: v0.9 根据菜单执行对应的业务，待实现。

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int
 }
 
 // 新建一个客户端
@@ -19,6 +20,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 	// 连接server
 	dial, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
@@ -28,6 +30,49 @@ func NewClient(serverIp string, serverPort int) *Client {
 	}
 	client.conn = dial
 	return client
+}
+
+// v0.9-菜单显示
+func (this *Client) menu() bool {
+	var flag int
+	fmt.Println("1. 公聊模式")
+	fmt.Println("2. 私聊模式")
+	fmt.Println("3. 更新用户名")
+	fmt.Println("0. 退出")
+
+	_, err := fmt.Scanln(&flag)
+	if err != nil {
+		return false
+	}
+
+	if flag >= 0 && flag <= 3 {
+		this.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>> 请输入合法范围内的数字。 <<<<<<<")
+		return false
+	}
+}
+
+// 根据输入执行对应的业务
+func (client Client) Run() {
+	for client.flag != 0 {
+		// 直到输入正确才不循环
+		for client.menu() != true {
+		}
+		// 根据不同的flag处理不同的业务
+		switch client.flag {
+		case 1:
+			// 公聊模式
+			break
+		case 2:
+			// 私聊模式
+			break
+		case 3:
+			// 更新用户名
+			break
+		}
+	}
 }
 
 var serverIP string
